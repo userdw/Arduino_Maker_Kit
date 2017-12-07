4b9703f6d33cf4de92067e6e26bc7754e10119ec
userdw <insidetrainingie@gmail.com>
2017-12-07 21:13:17 +0700

    Update Weather_Station.ino

--- 08_Weather_Station/Weather_Station/Weather_Station.ino
+++ 08_Weather_Station/Weather_Station/Weather_Station.ino
@@ -1,78 +1,25 @@
-#include <U8g2lib.h>
-#define MEASUREMENT_INTERVAL 5000
-
-//OLED pins CS, DC, RESET
-//OLED coordinate start at top-left corner from 0
-U8G2_SSD1306_128X64_VCOMH0_F_4W_HW_SPI oled(U8G2_R0, 10, 9, 8);
-int16_t measurement[5] = { -1, -1, -1, -1, -1};
-unsigned long oldTime = 30000;
-
-void drawLevelPage(int16_t *temperature) {
-  String temperatureStr = String(temperature[0]) + "°C";
-
-  oled.drawVLine(0, 16, 63);
-  oled.drawVLine(10, 16, 63);
-  for (byte i = 8; i < 32; i++) {
-    if (i % 2) {
-      oled.drawHLine(1, i * 2, 1);
-      oled.drawHLine(10 - 1, i * 2, 1);
-    }
-    else {
-      oled.drawHLine(1, i * 2, 2);
-      oled.drawHLine(10 - 2, i * 2, 2);
-    }
-  }
-
-  oled.setFont(u8g2_font_7x14B_mf);
-  byte xPosString = (oled.getDisplayWidth() / 2) - (oled.getStrWidth("Weather Station") / 2);
-  oled.drawStr(xPosString, 12, "Weather Station");
-
-  oled.drawHLine(0, map(temperature[0], 0, 100, 63, 16), 10);
-  xPosString = oled.getDisplayWidth() * 19 / 64 - oled.getStrWidth(temperatureStr.c_str()) / 2;
-  oled.drawStr(xPosString, 48, temperatureStr.c_str());
-
-  oled.drawFrame(oled.getDisplayWidth() / 2, 16, oled.getDisplayWidth() / 2, 48);
-  oled.setFont(u8g2_font_5x7_mf);
-
-  for (byte i = 1; i < 5; i++) {
-    if (temperature[i] == -1) temperatureStr = "NA";
-    else temperatureStr = String(temperature[i]) + "°C";
-    xPosString = oled.getDisplayWidth() * 3 / 4 - oled.getStrWidth(temperatureStr.c_str()) / 2;
-    oled.drawStr(xPosString, 28 + ((i - 1) * 10), temperatureStr.c_str());
-  }
-}
-
-void drawWelcomeMenu() {
-  oled.setFont(u8g2_font_crox5t_tf);
-  byte xPosString = (oled.getDisplayWidth() / 2) - (oled.getStrWidth("WELCOME") / 2);
-  oled.drawStr(xPosString, 43, "WELCOME");
-}
+ #define sensor A0
+float milliVoltage, temperature;
 
 void setup(void) {
-  oled.begin();
-  oled.clearBuffer();
-  drawWelcomeMenu();
-  oled.sendBuffer();
-  delay(2000);
+  pinMode (A0, INPUT);
+  Serial.begin(9600);
 }
 
+/*Rumus mencari suhu pada MCP9700
+ * Vout = (ADC / Resolusi ADC (1023)) x Vref (5V)
+ * dikurangi 500mv karena 0°C pada sensor MCP9700 adalah 500mv
+ * dibagi 10 karena typical sensor MCP9700 yaitu 10mV/°C
+ */
 void loop(void) {
-  if (millis() - oldTime >= MEASUREMENT_INTERVAL) {
-    float milliVoltage = analogRead(A0) * 3300.0 / 1023.0;
-    float temperature = (milliVoltage - 500) / 10;
+  milliVoltage = analogRead(sensor) * 5000.0 / 1023.0; 
+  temperature = (milliVoltage - 500) / 10;
+  
+  Serial.print("  ADC = ");
+  Serial.println(analogRead(A0));
 
-    if (temperature - (int)temperature > 0.5)
-      temperature = (int)temperature + 1;
-    else
-      temperature = (int)temperature;
-
-    for (byte i = 4; i > 0; i--)
-      measurement[i] = measurement[i - 1];
-    measurement[0] = (int)temperature;
-
-    oled.clearBuffer();
-    drawLevelPage(measurement);
-    oled.sendBuffer();
-    oldTime = millis();
-  }
+  Serial.print("  Suhu = ");
+  Serial.print(temperature);
+  Serial.println("°C");
+  delay(1000);
 }
