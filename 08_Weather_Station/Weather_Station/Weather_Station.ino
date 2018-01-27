dff2b1a9931402f271cb0591ce3a180f3bf9af7b
userdw <insidetrainingie@gmail.com>
2018-01-27 14:11:44 +0700

    Update Weather_Station.ino

--- 08_Weather_Station/Weather_Station/Weather_Station.ino
+++ 08_Weather_Station/Weather_Station/Weather_Station.ino
@@ -2,9 +2,9 @@
 float milliVoltage, temperature;
 
 void setup(void) {
-  pinMode (A0, INPUT);
   Serial.begin(9600);
 }
+
 void loop(void) {
   milliVoltage = analogRead(sensor) * 5000.0 / 1023.0; 
   temperature = (milliVoltage - 500) / 10;
